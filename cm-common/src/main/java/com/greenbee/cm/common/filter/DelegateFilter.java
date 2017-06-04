37b64596de92c2730d8dfa80dc21fcace7dc3d21
deng2 <dengyong.nju@gmail.com>
2017-06-04 22:08:07 +0800

    make no session creation for rest api

---
+++ cm-common/src/main/java/com/greenbee/cm/common/filter/DelegateFilter.java
@@ -0,0 +1,35 @@
+package com.greenbee.cm.common.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.web.filter.PathMatchingFilter;
+
+public class DelegateFilter extends PathMatchingFilter {
+
+    private String config_;
+
+    @Override
+    public Filter processPathConfig(String path, String config) {
+        config_ = config;
+        return super.processPathConfig(path, config);
+    }
+
+    @Override
+    protected boolean onPreHandle(ServletRequest request, ServletResponse response,
+            Object mappedValue) throws Exception {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpReq = (HttpServletRequest) request;
+            String path = httpReq.getServletPath();
+            if (path.startsWith(config_)) {
+                final String delegatePath = path.substring(config_.length());
+                request.getRequestDispatcher(delegatePath).forward(request, response);
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
