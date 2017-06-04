225201909e8d4323e5397da473ad60acc4370f88
deng2 <dengyong.nju@gmail.com>
2017-06-04 18:29:45 +0800

    add session and login tracker

---
+++ cm-common/src/main/java/com/greenbee/cm/common/track/LoginTracker.java
@@ -0,0 +1,35 @@
+package com.greenbee.cm.common.track;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationListener;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class LoginTracker implements AuthenticationListener {
+    private Log log_ = LogFactory.getLog(LoginTracker.class);
+
+    @Override
+    public void onSuccess(AuthenticationToken token, AuthenticationInfo info) {
+        if (log_.isInfoEnabled()) {
+            log_.info("Login success: " + token.getPrincipal());
+        }
+    }
+
+    @Override
+    public void onFailure(AuthenticationToken token, AuthenticationException ae) {
+        if (log_.isInfoEnabled()) {
+            log_.info("Login failure: " + token.getPrincipal());
+        }
+    }
+
+    @Override
+    public void onLogout(PrincipalCollection principals) {
+        if (log_.isInfoEnabled()) {
+            log_.info("Logout: " + principals.getPrimaryPrincipal());
+        }
+    }
+
+}
---
+++ cm-common/src/main/java/com/greenbee/cm/common/track/SessionTracker.java
@@ -0,0 +1,32 @@
+package com.greenbee.cm.common.track;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+
+public class SessionTracker implements SessionListener {
+    private Log log_ = LogFactory.getLog(SessionTracker.class);
+
+    @Override
+    public void onStart(Session session) {
+        if (log_.isInfoEnabled()) {
+            log_.info("Session started: " + session.getId() + "," + session.getHost());
+        }
+    }
+
+    @Override
+    public void onStop(Session session) {
+        if (log_.isInfoEnabled()) {
+            log_.info("Session stopped: " + session.getId());
+        }
+    }
+
+    @Override
+    public void onExpiration(Session session) {
+        if (log_.isInfoEnabled()) {
+            log_.info("Session expired: " + session.getId());
+        }
+    }
+
+}
